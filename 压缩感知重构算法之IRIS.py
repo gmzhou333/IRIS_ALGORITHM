32a137c1ae416a61694b981db6e9ad15f8d89e38
gmzhou333 <36431366+gmzhou333@users.noreply.github.com>
2018-02-21 15:47:42 +0800

    Update 压缩感知重构算法之IRIS.py

--- 压缩感知重构算法之IRIS.py
+++ 压缩感知重构算法之IRIS.py
@@ -1,1 +1,19 @@
-
+#IRLS算法函数
+def cs_irls(y,T_Mat):   
+    L=math.floor((y.shape[0])/4)
+    hat_x_tp=np.dot(T_Mat.T ,y)
+    epsilong=1
+    p=1 # solution for l-norm p
+    times=1
+    while (epsilong>10e-9) and (times<L):  #迭代次数
+        weight=(hat_x_tp**2+epsilong)**(p/2-1)
+        Q_Mat=np.diag(1/weight)
+        #hat_x=Q_Mat*T_Mat'*inv(T_Mat*Q_Mat*T_Mat')*y
+        temp=np.dot(np.dot(T_Mat,Q_Mat),T_Mat.T)
+        temp=np.dot(np.dot(Q_Mat,T_Mat.T),np.linalg.inv(temp))
+        hat_x=np.dot(temp,y)        
+        if(np.linalg.norm(hat_x-hat_x_tp,2) < np.sqrt(epsilong)/100):
+            epsilong = epsilong/10
+        hat_x_tp=hat_x
+        times=times+1
+    return hat_x
